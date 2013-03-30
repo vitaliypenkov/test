e5aa005ee53b69c67c42b59c5abbe12bd792a4bb
vitaliypenkov <vitaliy.penkov@gmail.com>
2013-03-30 12:24:28 -0400

    adding new fields. A bug when displaying existing skills

--- templates/skills.php
+++ templates/skills.php
@@ -15,12 +15,12 @@
          <?php
             $count = count($skills);          
            // var_dump($skills);   
-            //if any skills found, display prepopulated fields             
+            //if any skill found, display prepopulated fields             
             if (!empty($skills[0]))
             {
                 for ($i = 0; $i < $count; $i++)
                 {                       
-                    print ("<div class='control-group'>");
+                    print ("<div id=div$i class='control-group'>");
                     print ("<input type='checkbox' id = \"sel$i\" name = \"{$skills[$i]["id"]}\" />");                  
                     print ("<input id=\"skill$i\" name=\"skill_name[]\" value=\"{$skills[$i]["skill"]}\" type=\"text\"/>");                      
                     print ("<select id =\"level$i\" name=\"skill_exp[]\">");
@@ -60,7 +60,7 @@
             // if no skills found, display empty fields
             else
             {
-                    print ("<div class='control-group'>");
+                    print ("<div id=id0 class='control-group'>");
                     print ("<input type='checkbox' id = sel0 name = 'skill0_sel'/>");
                     print ("<input id=\"skill0\" name=\"skill_name[]\" placeholder=\"Please fill in technology\" type=\"text\"/>");  
                     print ("<select id =\"level0\" name=\"skill_exp[]\">");   
@@ -72,8 +72,7 @@
                     print ("</div>");
             }
          
-         ?>
-        
+         ?>        
 
         <div class="control-group">  
             <button type="button" id="add" onClick="add()" class="btn">Add More</button>
@@ -84,18 +83,36 @@
         </div>
     </fieldset>
 </form>
-
-<script type="text/javascript"><!--
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script>
+<!--
 $(document).ready(function() {
-  var new_btn = $('<button id="btn">Click</button>');          // new button
-  var new_span = $('<button id="btn">HA</button>');      // new <span> with class="spn"
+ // var new_btn = $('<button id="btn">Click</button>');          // new button
+  var new_btn = $('<button id="btn">HA</button>');      // new <span> with class="spn"
     var new_input = $('<input placeholder="HA" ></input>');
-  new_btn.insertAfter('#add');        // insert the new button after the tag with id="idd"
+    var new_div = $('<div id=div1 ></div>').addClass("control-group");
+    var new_check = $("<input type='checkbox' id = 'sel1' /\>");
+    var new_name = $("<input id=skill1 name=skill_name[] placeholder=\"Please fill in technology\" type=\"text\"/\>");
+   
+   // var new_exp = $("<option value=\"0\">Please Select</option>");
+   var new_exp = $("<select id =\"level1\" name=\"skill_exp[]\">");
+ // new_btn.insertAfter('#add');        // insert the new button after the tag with id="idd"
 
   // now we use the new added button, when is clicked
-  $('#btn').click(function() {
-    // insert the "new_span" at the beginning inside all DIVs with class="cls"
-    new_span.prepend('#add');
+  $('#add').click(function() {
+    // insert the "new_span" at the beginning inside all DIVs with class="cls"    
+    new_div.insertAfter('#div0');
+    new_check.appendTo('#div1');
+    new_name.appendTo('#div1');
+    new_exp.appendTo('#div1');
+    
+    selectValues = {"0": "Please select", "1": "Junior", "2": "Middle", "3": "Expert" };
+    $.each(selectValues, function(key, value) {   
+     $('#level1')
+          .append($('<option>', { value : key })
+          .text(value)); 
+    });
+    
   });
 });
 --></script>
