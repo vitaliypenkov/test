8b7b6e75fe7420db902c0d82314ab6fdbcc8fbae
vitaliypenkov <vitaliy.penkov@gmail.com>
2013-04-01 13:02:56 -0400

    DB sunday update

---
+++ templates/cv.sql
@@ -0,0 +1,213 @@
+-- MySQL dump 10.13  Distrib 5.5.28, for Linux (i686)
+--
+-- Host: localhost    Database: cv
+-- ------------------------------------------------------
+-- Server version	5.5.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `exp`
+--
+
+DROP TABLE IF EXISTS `exp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exp` (
+  `value` int(11) NOT NULL,
+  `name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exp`
+--
+
+LOCK TABLES `exp` WRITE;
+/*!40000 ALTER TABLE `exp` DISABLE KEYS */;
+INSERT INTO `exp` VALUES (1,'Junior'),(2,'Middle'),(3,'Expert');
+/*!40000 ALTER TABLE `exp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal_info`
+--
+
+DROP TABLE IF EXISTS `personal_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personal_info` (
+  `user_id` int(10) unsigned NOT NULL,
+  `fname` varchar(24) DEFAULT NULL,
+  `lname` varchar(24) DEFAULT NULL,
+  `mname` varchar(24) DEFAULT NULL,
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal_info`
+--
+
+LOCK TABLES `personal_info` WRITE;
+/*!40000 ALTER TABLE `personal_info` DISABLE KEYS */;
+INSERT INTO `personal_info` VALUES (12,'My12','Last','Name'),(15,'z','x','c');
+/*!40000 ALTER TABLE `personal_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `portfolio`
+--
+
+DROP TABLE IF EXISTS `portfolio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `portfolio` (
+  `id` int(11) NOT NULL,
+  `symbol` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `shares` int(11) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`,`symbol`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+LOCK TABLES `portfolio` WRITE;
+/*!40000 ALTER TABLE `portfolio` DISABLE KEYS */;
+INSERT INTO `portfolio` VALUES (7,'B','Barnes Group, Inc',6,27.83),(7,'DVN.V','',16,0.23);
+/*!40000 ALTER TABLE `portfolio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `skill` varchar(30) NOT NULL,
+  `exp` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skills`
+--
+
+LOCK TABLES `skills` WRITE;
+/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `transaction` varchar(10) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `symbol` varchar(16) NOT NULL,
+  `shares` int(11) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,7,'BUY','2013-03-10 17:15:20','A',1,43.03),(2,7,'BUY','2013-03-10 17:25:00','B',1,27.83),(3,7,'SELL','2013-03-10 17:28:41','A',2,43.03);
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (12,'a','$1$Nycxskor$JModYPY0MRZeQIT4oZodL1'),(14,'b','$1$VTZB1x9s$Z9jKwbLW7g/ORnjOfHbQd/'),(15,'c','$1$6OZYatiK$w.StmN9jN46sNwXnak/Sg.');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `work_exp`
+--
+
+DROP TABLE IF EXISTS `work_exp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `work_exp` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `company` varchar(64) NOT NULL,
+  `position` varchar(64) NOT NULL,
+  `start_date` varchar(32) NOT NULL,
+  `end_date` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=126 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `work_exp`
+--
+
+LOCK TABLES `work_exp` WRITE;
+/*!40000 ALTER TABLE `work_exp` DISABLE KEYS */;
+INSERT INTO `work_exp` VALUES (10,10,'rgahg','setrhasrt','sdhd','wrehae'),(122,12,'fgjfdxj','fvngfcgn','ghmgchm','ghcmgchm'),(123,12,'fgjfdxj','fvngfcgn','ghmgchm','ghcmgchm'),(124,12,'fgjfdxj','fvngfcgn','ghmgchm','ghcmgchm'),(125,12,'fgjfdxj','fvngfcgn','ghmgchm','ghcmgchm');
+/*!40000 ALTER TABLE `work_exp` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-04-01 13:02:20
